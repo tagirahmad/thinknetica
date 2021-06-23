77eff613844628c33ebcab1f1ccdb5746280fd85
tagirahmad <tagirbpl@gmail.com>
2021-06-23 19:32:06 +0300

    Lesson 3

---
+++ lesson 3 oop basics/oop_basics.rb
@@ -0,0 +1,82 @@
+class Station
+  def initialize(name)
+    @name = name
+    @trains = []
+  end
+
+  def accept_train(train)
+    @trains << train
+  end
+
+  def show_train_by_type(type)
+    @trains.select{|train| train.type == type}
+  end
+
+  def send_train(train)
+    @trains.delete(train)
+  end
+end
+
+class Route
+  attr_reader :starting_station
+
+  def initialize(starting_station, end_station)
+    @starting_station = starting_station
+    @end_station = end_station
+    @intermediate_stations = []
+  end
+
+  def add_intermediate_station(station)
+    @intermediate_stations << station
+  end
+
+  def delete_intermediate_station(station)
+    @intermediate_stations.delete(station)
+  end
+
+  def show_all_stations
+    [@starting_station, @intermediate_stations, @end_station].flatten!
+  end
+end
+
+class Train
+  attr_reader :speed, :wagon_count, :type
+
+  def initialize(train_number, type, wagon_count)
+    @train_number = train_number
+    @type = type
+    @wagon_count = wagon_count
+    @speed = 0
+    @route = nil
+  end
+
+  def increase_speed(speed)
+    @speed = speed
+  end
+
+  def stop
+    @speed = 0
+  end
+
+  def hitch_wagon
+    @wagon_count += 1 if @speed.zero?
+  end
+
+  def unhitch_wagon
+    @wagon_count -= 1 if @speed.zero?
+  end
+
+  def accept_route(route)
+    @route = route
+    @route.starting_station.accept_train(self)
+  end
+end
+
+route1 = Route.new(Station.new('station 1'), Station.new('station 2'))
+intermediate_station = Station.new('Intermediate station')
+route1.add_intermediate_station(intermediate_station)
+route1.delete_intermediate_station(intermediate_station)
+# p route1.show_all_stations
+train = Train.new('10', 'type1', 3)
+train.accept_route(route1)
+p route1.starting_station.show_train_by_type('type1')
